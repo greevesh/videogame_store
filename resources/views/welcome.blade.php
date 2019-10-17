1c7fce92b8bcd95e54839395646a795aebac83e2
Harrison Louis Greeves <hgreeves@protonmail.com>
2019-10-17 15:02:36 +0800

    Restyled some of the landing page.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,100 +1,127 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<!doctype html>
+<html lang="en">
     <head>
         <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Gamer</title>
 
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Play&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
         <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+        <link rel="stylesheet" href="https://laravelecommerceexample.ca/css/app.css">
+        <link rel="stylesheet" href="https://laravelecommerceexample.ca/css/responsive.css">
 
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
+        <div id="app">
+            <header class="with-background">
+                <div class="top-nav container">
+                    <div class="top-nav-left">
+                        <a href="/"><img src="{{ asset('images/gamer_logo.png') }}" alt="Gamer logo"></a>
+                        <ul>
+            <li>
+            <a href="/shop">
+                Shop
+                            </a>
+        </li>
+    </ul>
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
+                    </div>
+                    <div class="top-nav-right">
+                        <ul>
+        <li><a href="https://laravelecommerceexample.ca/register">Sign Up</a></li>
+    <li><a href="https://laravelecommerceexample.ca/login">Login</a></li>
+        <li><a href="https://laravelecommerceexample.ca/cart">Cart
+        </a></li>
+    
+</ul>
+                    </div>
+                </div> <!-- end top-nav -->
+            </header>
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
+            <div class="featured-section">
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
-        </div>
+                <div class="container">
+                    <h1 class="text-center">Laravel Ecommerce</h1>
+
+                    <p class="section-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore vitae nisi, consequuntur illum dolores cumque pariatur quis provident deleniti nesciunt officia est reprehenderit sunt aliquid possimus temporibus enim eum hic lorem.</p>
+
+                    <div class="text-center button-container">
+                        <a href="#" class="button">Featured</a>
+                        <a href="#" class="button">On Sale</a>
+                    </div>
+
+                    
+
+                    <div class="products text-center">
+                                                    <div class="product">
+                                <a href="https://laravelecommerceexample.ca/shop/appliance-5"><img src="https://laravelecommerceexample.ca/storage/products/dummy/appliance-5.jpg" alt="product"></a>
+                                <a href="https://laravelecommerceexample.ca/shop/appliance-5"><div class="product-name">Appliance 5</div></a>
+                                <div class="product-price">$823.15</div>
+                            </div>
+                                                    <div class="product">
+                                <a href="https://laravelecommerceexample.ca/shop/tv-4"><img src="https://laravelecommerceexample.ca/storage/products/dummy/tv-4.jpg" alt="product"></a>
+                                <a href="https://laravelecommerceexample.ca/shop/tv-4"><div class="product-name">TV 4</div></a>
+                                <div class="product-price">$1323.64</div>
+                            </div>
+                                                    <div class="product">
+                                <a href="https://laravelecommerceexample.ca/shop/tablet-5"><img src="https://laravelecommerceexample.ca/storage/products/dummy/tablet-5.jpg" alt="product"></a>
+                                <a href="https://laravelecommerceexample.ca/shop/tablet-5"><div class="product-name">Tablet 5</div></a>
+                                <div class="product-price">$982.63</div>
+                            </div>
+                                                    <div class="product">
+                                <a href="https://laravelecommerceexample.ca/shop/phone-2"><img src="https://laravelecommerceexample.ca/storage/products/dummy/phone-2.jpg" alt="product"></a>
+                                <a href="https://laravelecommerceexample.ca/shop/phone-2"><div class="product-name">Phone 2</div></a>
+                                <div class="product-price">$849.84</div>
+                            </div>
+                                                    <div class="product">
+                                <a href="https://laravelecommerceexample.ca/shop/phone-4"><img src="https://laravelecommerceexample.ca/storage/products/dummy/phone-4.jpg" alt="product"></a>
+                                <a href="https://laravelecommerceexample.ca/shop/phone-4"><div class="product-name">Phone 4</div></a>
+                                <div class="product-price">$1232.44</div>
+                            </div>
+                                                    <div class="product">
+                                <a href="https://laravelecommerceexample.ca/shop/tablet-3"><img src="https://laravelecommerceexample.ca/storage/products/dummy/tablet-3.jpg" alt="product"></a>
+                                <a href="https://laravelecommerceexample.ca/shop/tablet-3"><div class="product-name">Tablet 3</div></a>
+                                <div class="product-price">$886.73</div>
+                            </div>
+                                                    <div class="product">
+                                <a href="https://laravelecommerceexample.ca/shop/camera-7"><img src="https://laravelecommerceexample.ca/storage/products/dummy/camera-7.jpg" alt="product"></a>
+                                <a href="https://laravelecommerceexample.ca/shop/camera-7"><div class="product-name">Camera 7</div></a>
+                                <div class="product-price">$1648.05</div>
+                            </div>
+                                                    <div class="product">
+                                <a href="https://laravelecommerceexample.ca/shop/laptop-22"><img src="https://laravelecommerceexample.ca/storage/products/dummy/laptop-22.jpg" alt="product"></a>
+                                <a href="https://laravelecommerceexample.ca/shop/laptop-22"><div class="product-name">Laptop 22</div></a>
+                                <div class="product-price">$1800.45</div>
+                            </div>
+                        
+                    </div> <!-- end products -->
+
+                    <div class="text-center button-container">
+                        <a href="https://laravelecommerceexample.ca/shop" class="button">See more</a>
+                    </div>
+
+                </div> <!-- end container -->
+
+            </div> <!-- end featured-section -->
+
+            <footer>
+    <div class="footer-content container">
+        <ul>
+            <li><a href="#"><i class="fa fa-youtube"></i></a></li>
+            <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+            <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+            <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+        </ul>
+
+    </div> <!-- end footer-content -->
+</footer>
+
+        </div> <!-- end #app -->
+        <script src="js/app.js"></script>
     </body>
 </html>
