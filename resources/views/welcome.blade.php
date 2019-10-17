d9264afdbd515bbe62aab7cfcbb91ddab118efb0
Harrison Louis Greeves <hgreeves@protonmail.com>
2019-10-18 01:23:07 +0800

    Was in the wrong file path. Here's the updated landing page.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,81 +19,68 @@
     <body>
         <div id="app">
             <header class="with-background">
-                <div class="top-nav container">
-                    <div class="top-nav-left">
-                        <a href="/"><img src="{{ asset('images/gamer_logo.png') }}" alt="Gamer logo"></a>
-                        <ul>
-            <li>
-            <a href="/shop">
-                Shop
-                            </a>
-        </li>
-    </ul>
-
-                    </div>
+            <div style="height: 150px;" class="top-nav container">
+                <div class="top-nav-left">
+                    <div style="margin-top: -42px;"><a href="/"><img src="{{ asset('images/gamer_logo2.png') }}" alt="Gamer logo"></a></div>
+                </div>
                     <div class="top-nav-right">
                         <ul>
-        <li><a href="https://laravelecommerceexample.ca/register">Sign Up</a></li>
-    <li><a href="https://laravelecommerceexample.ca/login">Login</a></li>
-        <li><a href="https://laravelecommerceexample.ca/cart">Cart
-        </a></li>
-    
-</ul>
+                            <li><a href="#">Playstation</a></li>
+                            <li><a href="#">Xbox</a></li>
+                            <li><a href="#">Nintendo</a></li>
+                            <li><a href="#">PC</a></li>
+                            <li>|</li>
+                            <li><a href="https://laravelecommerceexample.ca/register">Sign Up</a></li>
+                            <li><a href="https://laravelecommerceexample.ca/login">Login</a></li>
+                            <li><a href="https://laravelecommerceexample.ca/cart">Cart</a></li>
+                        </ul>
                     </div>
-                </div> <!-- end top-nav -->
-            </header>
+                </div>
+            </div> <!-- end top-nav -->
+        </header>
 
             <div class="featured-section">
 
                 <div class="container">
-                    <h1 class="text-center">Laravel Ecommerce</h1>
-
-                    <p class="section-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore vitae nisi, consequuntur illum dolores cumque pariatur quis provident deleniti nesciunt officia est reprehenderit sunt aliquid possimus temporibus enim eum hic lorem.</p>
-
-                    <div class="text-center button-container">
-                        <a href="#" class="button">Featured</a>
-                        <a href="#" class="button">On Sale</a>
-                    </div>
-
-                    
+                    <h1 class="text-center">Deals</h1>                    
 
                     <div class="products text-center">
-                                                    <div class="product">
+                            <div class="product">
                                 <a href="https://laravelecommerceexample.ca/shop/appliance-5"><img src="https://laravelecommerceexample.ca/storage/products/dummy/appliance-5.jpg" alt="product"></a>
                                 <a href="https://laravelecommerceexample.ca/shop/appliance-5"><div class="product-name">Appliance 5</div></a>
                                 <div class="product-price">$823.15</div>
                             </div>
-                                                    <div class="product">
+                            <div class="product">
                                 <a href="https://laravelecommerceexample.ca/shop/tv-4"><img src="https://laravelecommerceexample.ca/storage/products/dummy/tv-4.jpg" alt="product"></a>
                                 <a href="https://laravelecommerceexample.ca/shop/tv-4"><div class="product-name">TV 4</div></a>
                                 <div class="product-price">$1323.64</div>
                             </div>
-                                                    <div class="product">
+                            <div class="product">
                                 <a href="https://laravelecommerceexample.ca/shop/tablet-5"><img src="https://laravelecommerceexample.ca/storage/products/dummy/tablet-5.jpg" alt="product"></a>
                                 <a href="https://laravelecommerceexample.ca/shop/tablet-5"><div class="product-name">Tablet 5</div></a>
                                 <div class="product-price">$982.63</div>
                             </div>
-                                                    <div class="product">
+                            <div class="product">
                                 <a href="https://laravelecommerceexample.ca/shop/phone-2"><img src="https://laravelecommerceexample.ca/storage/products/dummy/phone-2.jpg" alt="product"></a>
                                 <a href="https://laravelecommerceexample.ca/shop/phone-2"><div class="product-name">Phone 2</div></a>
                                 <div class="product-price">$849.84</div>
                             </div>
-                                                    <div class="product">
+                            <div class="product">
                                 <a href="https://laravelecommerceexample.ca/shop/phone-4"><img src="https://laravelecommerceexample.ca/storage/products/dummy/phone-4.jpg" alt="product"></a>
                                 <a href="https://laravelecommerceexample.ca/shop/phone-4"><div class="product-name">Phone 4</div></a>
                                 <div class="product-price">$1232.44</div>
                             </div>
-                                                    <div class="product">
+                            <div class="product">
                                 <a href="https://laravelecommerceexample.ca/shop/tablet-3"><img src="https://laravelecommerceexample.ca/storage/products/dummy/tablet-3.jpg" alt="product"></a>
                                 <a href="https://laravelecommerceexample.ca/shop/tablet-3"><div class="product-name">Tablet 3</div></a>
                                 <div class="product-price">$886.73</div>
                             </div>
-                                                    <div class="product">
+                            <div class="product">
                                 <a href="https://laravelecommerceexample.ca/shop/camera-7"><img src="https://laravelecommerceexample.ca/storage/products/dummy/camera-7.jpg" alt="product"></a>
                                 <a href="https://laravelecommerceexample.ca/shop/camera-7"><div class="product-name">Camera 7</div></a>
                                 <div class="product-price">$1648.05</div>
                             </div>
-                                                    <div class="product">
+                            <div class="product">
                                 <a href="https://laravelecommerceexample.ca/shop/laptop-22"><img src="https://laravelecommerceexample.ca/storage/products/dummy/laptop-22.jpg" alt="product"></a>
                                 <a href="https://laravelecommerceexample.ca/shop/laptop-22"><div class="product-name">Laptop 22</div></a>
                                 <div class="product-price">$1800.45</div>
@@ -104,24 +91,19 @@
                     <div class="text-center button-container">
                         <a href="https://laravelecommerceexample.ca/shop" class="button">See more</a>
                     </div>
-
                 </div> <!-- end container -->
-
-            </div> <!-- end featured-section -->
+            </div> <!-- end deal section -->
 
             <footer>
-    <div class="footer-content container">
-        <ul>
-            <li><a href="#"><i class="fa fa-youtube"></i></a></li>
-            <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-            <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-            <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
-        </ul>
-
-    </div> <!-- end footer-content -->
-</footer>
-
-        </div> <!-- end #app -->
-        <script src="js/app.js"></script>
+                <div class="footer-content container">
+                    <ul style="margin-left: 420px;">
+                        <li><a href="#"><i class="fa fa-youtube"></i></a></li>
+                        <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                        <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                        <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                    </ul>
+                </div> 
+            </footer> <!-- end footer-content -->
+        </div> 
     </body>
 </html>
