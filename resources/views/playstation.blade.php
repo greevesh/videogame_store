01745706d59c5c32c04441bef28886955d72864d
Harrison Louis Greeves <hgreeves@protonmail.com>
2019-10-27 00:10:13 +0800

    Reinstalled Voyager to try and solve the login problem. Unsuccessful. Removed the descriptions from the playstation view too.

--- resources/views/playstation.blade.php
+++ resources/views/playstation.blade.php
@@ -4,15 +4,17 @@
     <div class="featured-section">
 
         <div class="container">
-            <h1 class="text-center">Playstation 4 Games</h1>&nbsp;                  
+            <h1 class="text-center">Playstation 4 Games</h1>
+            <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt debitis, amet magnam accusamus nisi distinctio eveniet ullam. Facere, cumque architecto.</h4>
+            &nbsp;
 
-        <div class="products text-center">
-            <div class="product">
+        <div class="text-center">
+            <div>
                 @foreach($playstationGames as $playstationGame)
                     <h2>{{ $playstationGame->name }}</h2>
-                    <a href=""><img src="{{ asset($playstationGame->image) }}" alt="product"></a>
+                    <a href=""><img src="{{ asset($playstationGame->image) }}" alt="playstationgame"></a>
                     <h4>{{ $playstationGame->price }}</h4>
-                    <h4>{{ $playstationGame->description}}</h4>
                 @endforeach  
-        </div> 
+            </div> 
+        </div>
 @endsection
