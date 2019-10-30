80a06717e5d5d349816cdbc1efe21cb166259fa0
Harrison Louis Greeves <hgreeves@protonmail.com>
2019-10-30 14:18:38 +0800

    The main routes have been configured. Most of the navbar and footer styling have also been completed.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Template') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -18,13 +18,16 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    <!-- loads fa with javascript before the rest of the page has loaded to prevent slow loading times -->
+    <script type="text/javascript"> (function() { var css = document.createElement('link'); css.href = 'https://use.fontawesome.com/releases/v5.1.0/css/all.css'; css.rel = 'stylesheet'; css.type = 'text/css'; document.getElementsByTagName('head')[0].appendChild(css); })(); </script>
 </head>
 <body>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    {{ config('app.name', 'Template') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -41,6 +44,21 @@
                         <!-- Authentication Links -->
                         @guest
                             <li class="nav-item">
+                                <a class="nav-link" href="{{ route('example1') }}">{{ ('Example 1') }}</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('example2') }}">{{ ('Example 2') }}</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('example3') }}">{{ ('Example 3') }}</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('example4') }}">{{ ('Example 4') }}</a>
+                            </li>
+                            <li>
+                                
+                            </li>
+                            <li class="nav-item">
                                 <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                             </li>
                             @if (Route::has('register'))
@@ -48,6 +66,7 @@
                                     <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                                 </li>
                             @endif
+                            <li><a href="{{ route('cart') }}"><i class="fas fa-shopping-cart"></i></a></li>
                         @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
@@ -75,6 +94,17 @@
         <main class="py-4">
             @yield('content')
         </main>
+
+        <footer>
+            <div class="footer-content container">
+                <ul style="margin-left: 420px;">
+                    <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                    <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
+                    <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                    <li><a href="#"><i class="fab fa-youtube"></i></a></li>
+                </ul>
+            </div> 
+        </footer> 
     </div>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,15 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'landingpage');
+// making use of named helpers in case we want to change the route url and not have to change every instance
+Route::view('/example1', 'products/example1')->name('example1');
+Route::view('/example2', 'products/example2')->name('example2');
+Route::view('/example3', 'products/example3')->name('example3');
+Route::view('/example4', 'products/example4')->name('example4');
+Route::view('/cart', 'cart')->name('cart');
 
+// login and register
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
