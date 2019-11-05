69ff4943d6b87979fed0afaabe931e4a50bdb94c
Harrison Louis Greeves <hgreeves@protonmail.com>
2019-11-05 23:34:46 +0800

    Added the logo. Landing page has been updated with a background image and a pre-order button. Margin and padding around the image still needs to be removed.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,9 +26,7 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Gamer') }}
-                </a>
+                <a href="/"><img style="margin-bottom: 0.8rem" src="{{ asset('storage/images/gamer_logo2.png') }}" alt="Gamer logo"></a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
