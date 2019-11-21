be0613e12a869d9edbb045a583263d06e0b7175c
Harrison Louis Greeves <hgreeves@protonmail.com>
2019-11-21 13:05:24 +0800

    Got the playstation products to render with 2 on a line. There's still a big space between the footer and the final two products.

--- app/Game.php
+++ app/Game.php
@@ -3,15 +3,10 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-use Treestoneit\ShoppingCart\Buyable;
-use Treestoneit\ShoppingCart\BuyableTrait;
 
-class Game extends Model implements Buyable
+class Game extends Model
 {
     protected $primaryKey = 'game_id'; 
-    
-    // enables shopping cart functionality
-    use BuyableTrait; 
 
     protected $fillable = ['name'];
 
--- resources/views/products/playstation.blade.php
+++ resources/views/products/playstation.blade.php
@@ -4,12 +4,22 @@
 @section('category', 'Playstation')
 
 @section('content')
-    <h1 class="d-flex justify-content-center">Playstation Games</h1>
+    <h1 class="d-flex justify-content-center" style="padding-bottom: 5rem;">Playstation Games</h1>
     @if(isset($playstationGames))
         @foreach($playstationGames as $playstationGame)
-            <h2>{{ $playstationGame->name }}</h2>
-            <a href="/playstation/{{ $playstationGame->slug }}"><img src="{{ asset($playstationGame->image) }}" alt="playstationgame"></a>
-            <h4>{{ $playstationGame->price }}</h4>
+            @if($playstationGame->game_id % 2 === 0)
+                <div class="text-right" style="margin-top: -26.6rem; margin-right: 10rem; padding-bottom: 2rem;">
+                    <h2>{{ $playstationGame->name }}</h2>
+                    <a href="/playstation/{{ $playstationGame->slug }}"><img src="{{ asset($playstationGame->image) }}" alt="playstationgame"></a>
+                    <h4 style="margin-right: 6.2rem;">£{{ $playstationGame->price }}</h4>
+                </div>
+            @else
+                <div class="text-left" style="margin-left: 10rem;">
+                    <h2>{{ $playstationGame->name }}</h2>
+                    <a href="/playstation/{{ $playstationGame->slug }}"><img src="{{ asset($playstationGame->image) }}" alt="playstationgame"></a>
+                    <h4 style="margin-left: 6.2rem;">£{{ $playstationGame->price }}</h4>
+                </div>
+            @endif 
         @endforeach 
     @endif
 @endsection
