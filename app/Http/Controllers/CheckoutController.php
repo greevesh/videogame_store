f5c1809d7b3eec4244063bc1e19dd9e700030123
Harrison Louis Greeves <hgreeves@protonmail.com>
2020-01-26 01:28:20 +0800

    User is now validated more accurately during checkout. More customer details are also passed onto the Stripe backend during checkout.

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -9,47 +9,10 @@ use Illuminate\Support\Facades\Auth;
 
 class CheckoutController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
-        if (Auth::check()) 
-        {
-            // the name and the email of the logged in user can not be validated here
-            // as the inputs for those credentials have automatically been filled by the app
-            request()->validate([
-                'address' => 'required|min:10',
-                'address2',
-                'country' => 'required',
-                'postcode' => 'required',
-                'card-name' => 'required'
-            ]);
-        }
-        else
+        // if user hasn't logged in
+        if (!Auth::check()) 
         {
             request()->validate([
                 'name' => 'required|min:5',
@@ -61,6 +24,17 @@ class CheckoutController extends Controller
                 'card-name' => 'required'
             ]);
         }
+            // the name and the email of the logged in user can not be validated here
+            // as the inputs for those credentials would have been automatically filled by the app
+            // if the user is logged in
+            request()->validate([
+                'address' => 'required|min:10',
+                'address2',
+                'country' => 'required',
+                'postcode' => 'required',
+                'card-name' => 'required'
+            ]);
+            
 
         // storing Stripe data
         $stripe = new Stripe();
@@ -68,19 +42,22 @@ class CheckoutController extends Controller
 
         try {
             $charge = Stripe::charges()->create([
-                // 'amount' => getNumbers()->get('newTotal') / 100,
                 'amount' => Cart::total(),
                 'currency' => 'GBP',
                 'source' => $request->stripeToken,
                 'description' => 'Thank you for your purchase.',
                 'receipt_email' => $request->email,
                 'metadata' => [
-                    // 'contents' => $contents,
-                    'quantity' => Cart::count(),
+                    'Quantity' => Cart::count(),
+                    'Name' => Auth::check() ? Auth::user()->name : $request->name,
+                    'Email' => Auth::check() ? Auth::user()->email : $request->email,
+                    'Address' => $request->address, 
+                    'Country' => $request->country,
+                    'Postcode' => $request->postcode
                 ],
             ]);
 
-            $customer = $stripe->customers()->create(['email' => 'john@doe.com']);
+            $customer = $stripe->customers()->create(['email' => $request->email]);
 
             Cart::destroy();
 
@@ -91,50 +68,5 @@ class CheckoutController extends Controller
             catch (CardErrorException $e) {
             return back()->withErrors('Error! ' . $e->getMessage());
         }
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
     }
 }
