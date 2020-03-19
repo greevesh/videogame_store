e19f97d8da5defe15a4eea1b9d6e01d647042d62
Harrison Louis Greeves <hgreeves@protonmail.com>
2020-03-19 17:06:22 +0800

    Removed the logo margin.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,7 +26,7 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a href="/"><img id="navbar-logo" style="margin-bottom: 0.8rem" src="{{ asset('storage/images/gamer_logo2.png') }}" alt="Gamer logo"></a>
+                <a href="/"><img id="navbar-logo" src="{{ asset('storage/images/gamer_logo2.png') }}" alt="Gamer logo"></a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
