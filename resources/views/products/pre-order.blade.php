48641aea3a2c4dfe457035cb8997fb73900a2998
Harrison Louis Greeves <hgreeves@protonmail.com>
2020-03-20 12:01:11 +0800

    Pre-order page is more responsive.

--- resources/views/products/pre-order.blade.php
+++ resources/views/products/pre-order.blade.php
@@ -8,7 +8,7 @@
     @foreach($preOrderGames as $preOrderGame)
         @if(isset($preOrderGame))
             @if($preOrderGame->name === 'Pokemon Sword')
-                <div class="mt-5" id="pokemon-sword">
+                <div id="pokemon-sword">
                     <h2>{{ $preOrderGame->name }}</h2>
                     <img src="{{ asset($preOrderGame->image) }}" alt="pre-ordergame">
                     <h4>£{{ $preOrderGame->price }}</h4>
@@ -43,7 +43,7 @@
         @endif
     @endforeach
 
-    <p id="pre-order-description" class="text-center mt-4 pl-5 pr-5" style="font-size: 1rem;">
+    <p id="pre-order-description" class="text-center pl-5 pr-5" style="font-size: 1rem; margin-top: 5%;">
         A whole new Pokémon RPG adventure awaits in Pokémon Sword and Pokémon Shield on Nintendo Switch! Get ready to discover Galar, an expansive region with diverse environments. 
         The people and Pokémon who live here work together closely to develop the industries within it.
         Start your adventure in the new region inspired by the English countryside: Galar. With one of the 3 new starter Pokémon: Grookey the Grass type, Scorbunny the fire type, or Sobble the water type. 
